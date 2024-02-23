eb66d40daf7855665fe834ee04ca14abbd873215
prateek <prateeksingh8841@gmail.com>
2024-02-23 12:59:01 -0700

    feat: examples

--- src/MyButton.jsx
+++ src/MyButton.jsx
@@ -1,5 +1,7 @@
 export default function MyButton() {
-    return (
-        <button>I'm a button</button>
-    );
+  function handleClick() {
+    alert("clicked me");
+  }
+
+  return <button onClick={handleClick}>Click me</button>;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 import MyApp from './MyApp';
 import Profile from './Profile';
 import List from './List';
+import MyButton from './MyButton';
 
 ReactDOM.createRoot(
     document.querySelector('#root')
-).render(<List />);
+).render(<MyButton />);
