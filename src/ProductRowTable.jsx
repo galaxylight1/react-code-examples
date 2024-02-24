0bb308ade82c9d5bc0cd126e3cc0430a1cdbbe33
prateek <prateeksingh8841@gmail.com>
2024-02-23 17:57:49 -0700

    feat: examples

--- src/ProductRowTable.jsx
+++ src/ProductRowTable.jsx
@@ -11,20 +11,45 @@ export default function ProductRowTable({ products }) {
   });
 
   return (
-    <table>
-      <thead>
-        <tr>Fruits</tr>
-      </thead>
-      <tbody>
-        {fruits.map(fruit => 
-          <tr>
-            <th style={{
-                color: fruit.stocked ? 'black' : 'red'
-            }}>{fruit.name}</th>
-            <th>{fruit.price}</th>
-          </tr>
-        )}
-      </tbody>
-    </table>
+    <>
+      <table>
+        <thead>
+          <tr>Fruits</tr>
+        </thead>
+        <tbody>
+          {fruits.map((fruit) => (
+            <tr>
+              <th
+                style={{
+                  color: fruit.stocked ? "black" : "red",
+                }}
+              >
+                {fruit.name}
+              </th>
+              <th>{fruit.price}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>Vegetables</tr>
+        </thead>
+        <tbody>
+          {vegetables.map((vegetable) => (
+            <tr>
+              <th
+                style={{
+                  color: vegetable.stocked ? "black" : "red",
+                }}
+              >
+                {vegetable.name}
+              </th>
+              <th>{vegetable.price}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
