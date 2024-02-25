53dbff16bf88e687cf19e3f8b4b05fa642507d76
prateek <prateeksingh8841@gmail.com>
2024-02-25 01:37:31 -0700

    feat: examples

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 export const Users = () => {
-    const fetchUsers = () => {
-        fetch('http://localhost:4001/api/users') // promise chaining
-        .then(res => res.json())
-        .then((val) => {
-            console.log(val);
-        })
-        .catch(err => console.log(err));
-    };
-    fetchUsers();
-    return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  const[fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:4001/api/users") // promise chaining
+      .then((res) => res.json())
+      .then((val) => {
+        console.log(val);
+        setUsers(val);
+      })
+      .catch((err) => console.log(err));
+  }, [fetching]);
+
+  return (
+    <div>
+      {users.map((user, idx) => (
+        <p key={idx}>Username: {user.user}</p>
+      ))}
+      <button onClick={() => setFetching(!fetching)}>Fetch</button>
+    </div>
+  );
 };
